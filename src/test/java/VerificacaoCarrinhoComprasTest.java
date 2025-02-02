f48a639d5fc7804a2f9bfd6fc9d617d9669da503
David Medeiros <davidmedeiros94@hotmail.com>
2025-02-02 08:27:29 -0300

    otimizado o código e ajustes

---
+++ src/test/java/VerificacaoCarrinhoComprasTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VerificacaoCarrinhoComprasTest {
+  private WebDriver driver;
+
+  @BeforeEach
+  public void setUp() {
+    driver = new ChromeDriver();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    driver.quit();
+  }
+
+  @Test
+  public void verificacaoCarrinhoCompras() {
+    driver.get("https://www.saucedemo.com/");
+    driver.manage().window().setSize(new Dimension(1062, 804));
+    driver.findElement(By.cssSelector("*[data-test=\"username\"]")).click();
+    driver.findElement(By.cssSelector("*[data-test=\"username\"]")).sendKeys("standard_user");
+    driver.findElement(By.cssSelector("*[data-test=\"password\"]")).click();
+    driver.findElement(By.cssSelector("*[data-test=\"password\"]")).sendKeys("secret_sauce");
+    driver.findElement(By.cssSelector("*[data-test=\"login-button\"]")).click();
+    driver.findElement(By.cssSelector("*[data-test=\"add-to-cart-sauce-labs-backpack\"]")).click();
+    driver.findElement(By.cssSelector("*[data-test=\"shopping-cart-link\"]")).click();
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"item-quantity\"]")).getText(), ("1"));
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"inventory-item-name\"]")).getText(), ("Sauce Labs Backpack"));
+    assertEquals(driver.findElement(By.cssSelector("*[data-test=\"inventory-item-price\"]")).getText(), ("$29.99"));
+  }
+}
